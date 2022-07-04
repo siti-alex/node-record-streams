1db6259fcbb719cc1cb8303871bf9d1defcf33a4
asitikovdo <asitikovdo@gmail.com>
2022-07-04 15:31:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Description
+A simple script for recording live broadcasts from youtube and twitch
+
+##Critical dependencies
+```
+node.js
+python 3.7 in PATH
+ffmpeg in PATH
+youtube-dl it PATH
+```
+##Youtube-dl
+```
+$ pip install youtube-dl
+$ pip install --upgrade youtube-dl
+```
+
+##Running
+```
+$ node record.js <url>
+```
---
+++ record.js
@@ -0,0 +1,38 @@
+// console.log(process.pid)
+const ch_process = require('child_process')
+const os = require('os');
+
+async function getHLS(url) {
+    const output = await ch_process.spawn(
+        `youtube-dl`,
+        [
+            '-g',
+            `${url}`
+        ]
+    );
+    return output;
+}
+
+async function downloadStream(url){
+    const out = ch_process.spawn(
+        `ffmpeg`,
+        [
+            '-i',
+            `${url}`,
+            '-c',
+            'copy',
+            `out${process.pid}.ts`,
+            ]
+    );
+    console.log('Пишем...')
+    return out;
+}
+getHLS(process.argv[process.argv.length-1]).then(output => {
+    output.stdout.on('data', data=>{
+        // console.log(data.toString())
+        console.log(process.pid)
+        downloadStream(data.toString())
+    })
+})
+
+
